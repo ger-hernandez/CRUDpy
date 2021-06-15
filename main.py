aeaa66b35a2bddc7c5c1a837cc1973e5d3f20df4
German Hernandez <gerhernandez7@gmail.com>
2021-06-14 20:21:54 -0500

    add read and update function, the read function allow us to read the tasks in the dictionary and the update function allow us to update the tasks

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 #CRUD App - Pendient tasks
 def create(tareas, identificador, tareaNueva):
-        tareas[identificador] = tareaNueva
-        return tareas
+    tareas[identificador] = tareaNueva
+    return tareas
+
+def read(tareas):
+    for identificador in tareas.items():
+        print(identificador, '-', end='')
+        for nombre_atributo, atributo in tareas.items():
+            print(atributo, '-', end='')
+            print()
+
+def estaElemento(tareas, identificador):
+    conjunto = set(tareas.keys())
+    return identificador in conjunto
 
 tareas = {
     '01': {
@@ -35,6 +46,7 @@ while(True):
     option = int(input('ingrese una opcion: '.capitalize()))
     #create
     if(option == 1):
+        print()
         print('adicionando tarea'.title().center(50, '-'))
         identificador = input('ingrese identificador de la tarea: '.title())
         descripcion = input('ingrese descripcion de la tarea: '.title())
@@ -45,10 +57,29 @@ while(True):
             'descripcion': descripcion,
             'estado': estado,
             'tiempo': tiempo
-       }
+        }
 
         tareas = create(tareas, identificador, tareaNueva)
-    if option == 5:
+    elif(option == 2):
+        print()
+        print('listado de tareas'.title().center(50, '-'))
+        print()
+        read(tareas)
+    elif(option == 3):
+        print()
+        print('actualizar tarea'.title().center(50, '-'))
+        identificador = input('ingrese identificador  de la tarea a modificar'.title())
+        if(estaElemento(tareas, identificador)):
+            new_description = input('inserte nueva descripcion'.title())
+            tareas[identificador]['descripcion'] = new_description
+            new_status = input('inserte nuevo estado'.title())
+            tareas[identificador]['estado'] = new_status
+            new_time = int(input('ingrese nuevo tiempo'.title()))
+            tareas[identificador]['tiempo'] = new_time
+        else:
+            print('Esa Tarea no Existe')
+    elif(option == 4):
+        pass
+    elif option == 5:
         break
-
 print(tareas)
